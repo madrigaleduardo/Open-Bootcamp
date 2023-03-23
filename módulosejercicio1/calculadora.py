283724b9531016ba6cbdbcd372a7df97cf44dd52
Eduardo Madrigal <124392413+madrigaleduardo@users.noreply.github.com>
2023-03-22 20:08:33 -0600

    Add files via upload

---
+++ módulosejercicio1/calculadora.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "No se puede dividir entre cero"
+    else:
+        return a / b
---
+++ módulosejercicio1/main.py
@@ -0,0 +1,14 @@
+import calculadora
+
+resultado_suma = calculadora.sumar(29, 33)
+resultado_resta = calculadora.restar(52, 62)
+resultado_multiplicacion = calculadora.multiplicar(19, 4)
+resultado_division = calculadora.dividir(1000, 0)
+
+print("El resultado de la suma es:", resultado_suma)
+print("El resultado de la resta es:", resultado_resta)
+print("El resultado de la multiplicación es:", resultado_multiplicacion)
+print("El resultado de la división es:", resultado_division)
+
+
+
